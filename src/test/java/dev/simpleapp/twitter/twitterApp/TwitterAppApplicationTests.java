3f7234c0a244a38aa63be01841f1f1d5b279a235
Mikhail Belozerov <belozerov.mg@gmail.com>
2024-03-15 13:10:35 +0700

    First config project

---
+++ src/test/java/dev/simpleapp/twitter/twitterApp/TwitterAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.simpleapp.twitter.twitterApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
