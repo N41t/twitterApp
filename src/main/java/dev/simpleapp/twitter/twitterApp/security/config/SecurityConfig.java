6f85b4052569fed898aedbcf3a8c05c16fe23648
Mikhail Belozerov <belozerov.mg@gmail.com>
2024-03-15 17:22:24 +0700

    Inject PasswordEncoder

--- src/main/java/dev/simpleapp/twitter/twitterApp/security/config/SecurityConfig.java
+++ src/main/java/dev/simpleapp/twitter/twitterApp/security/config/SecurityConfig.java
@@ -6,6 +6,8 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -23,5 +25,10 @@ public class SecurityConfig {
                 .httpBasic(Customizer.withDefaults());
 
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
--- src/main/java/dev/simpleapp/twitter/twitterApp/security/web/UserAccountController.java
+++ src/main/java/dev/simpleapp/twitter/twitterApp/security/web/UserAccountController.java
@@ -1,14 +1,29 @@
 package dev.simpleapp.twitter.twitterApp.security.web;
 
+import dev.simpleapp.twitter.twitterApp.security.service.UserAccountService;
+import dev.simpleapp.twitter.twitterApp.security.service.UserRoleService;
 import dev.simpleapp.twitter.twitterApp.security.web.model.RegisterRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
 @RequestMapping("/api/v1/accounts")
 public class UserAccountController {
+
+    private final UserAccountService userAccountService;
+    private final UserRoleService userRoleService;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserAccountController(UserAccountService userAccountService,
+                                 UserRoleService userRoleService,
+                                 PasswordEncoder passwordEncoder) {
+        this.userAccountService = userAccountService;
+        this.userRoleService = userRoleService;
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @PostMapping("/register")
     @ResponseStatus(HttpStatus.CREATED)
